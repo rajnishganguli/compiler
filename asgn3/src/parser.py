f75430fadcb13587248ec6a0f5b1cf1574c5b6ef
Rajnish Ganguli <rkgangulii@gmail.com>
2018-03-14 03:50:59 +0530

    Categorized the productions

--- asgn3/src/parser.py
+++ asgn3/src/parser.py
@@ -7,15 +7,19 @@ from Lexer import tokens
 import re
 import derivation as drv
 
+############################################################# POSSIBLE STARTS ###############################
 def p_start(p):
     '''start : expression_statements'''
 
+############################################################# EXPRESSION STSTEMENTS ########################
 def p_expression_statements_1(p):
     '''expression_statements : expression start'''
 
 def p_expression_statements_2(p):
     '''expression_statements : expression'''
 
+
+############################################################# EXPRESSION TYPES #############################
 def p_expression_math_1(p):
     '''expression : IDENTIFIER OP_ASGN rightside'''
 
@@ -26,6 +30,7 @@ def p_jump_statements(p):
     '''jump_statements :  KEYWORD_BREAK
                        | KEYWORD_NEXT '''
 
+########################################################### RIGHTSIDE ######################################
 def p_math_rightside_1(p):
     '''rightside : TYPE_NUMERIC math rightside
                  | TYPE_INTEGER math rightside   
@@ -34,20 +39,36 @@ def p_math_rightside_1(p):
 def p_math_rightside_2(p):
     '''rightside : vector'''
 
-def p_math(p):
-    '''math : OP_PLUS
-            | OP_MINUS
-            | OP_MULT
-            | OP_DIVIDE
-            | OP_EXPO
-            | OP_REMDR'''
+########################################################### MATH OPERATOR ##################################
+def p_math_1(p):
+    '''math : OP_PLUS '''
+        
+def p_math_2(p):
+    '''math : OP_MINUS '''
+        
+def p_math_3(p):
+    '''math : OP_MULT '''
+        
+def p_math_4(p):
+    '''math : OP_DIVIDE '''
+        
+def p_math_5(p):
+    '''math : OP_EXPO '''
+        
+def p_math_6(p):
+    '''math : OP_REMDR '''
 
+
+########################################################### VECTOR #########################################
 def p_vector_1(p):
     '''vector : any_type '''
 
 def p_vector_2(p):
     '''vector : TYPE_NUMERIC SEP_COLON TYPE_NUMERIC'''
 
+
+
+########################################################## ANY_TYPE ########################################
 def p_any_type_1(p):
     '''any_type : TYPE_NUMERIC'''
 
@@ -60,6 +81,8 @@ def p_any_type_3(p):
 def p_any_type_4(p):
     '''any_type : TYPE_BOOLEAN '''
 
+
+######################################################### ERROR PRODUCTION ##################################
 def p_error(p):
     print("Syntax error in input!")
 
