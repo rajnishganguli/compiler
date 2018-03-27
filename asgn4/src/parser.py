6edc1cb23d9b47718f50e7d2db56603a63b89cb0
Jitendra <jnagar@iitk.ac.in>
2018-03-27 23:50:36 +0530

    irgen for more exp

--- asgn4/src/parser.py
+++ asgn4/src/parser.py
@@ -14,16 +14,15 @@ filename = sys.argv[1]
 def p_program(p):
     '''program : start'''
     TAC.emit('EndOfCode', [])
+
 def p_start(p):
     '''start : expression_statements'''
-    p[0] = p[1]
+
 def p_expression_statements_1(p):
     '''expression_statements : expression start'''
-    p[0] = [p[1]] + p[2]
 
 def p_expression_statements_2(p):
     '''expression_statements : expression'''
-    p[0] = p[1]
 
 def p_expression_math_1(p):
     '''expression : IDENTIFIER OP_ASGN rightside'''
@@ -37,34 +36,78 @@ def p_expression_math_1(p):
     p[0] = {"place":p[1], "type": var_dict["type"]} 
 
 def p_expression_math_2(p):
-    '''expression : rightside
-                  | jump_statements'''
-    p[0] = p[1]
-
-def p_jump_statements(p):
-    '''jump_statements :  KEYWORD_BREAK
-                       | KEYWORD_NEXT '''
-    p[0] = p[1]
-
+    '''expression : KEYWORD_PRINT BR_LCIR rightside BR_RCIR'''
+    TAC.emit('print',[p[3]["place"]])
+    
 def p_math_rightside_1(p):
     '''rightside : TYPE_NUMERIC math rightside'''
-    if(p[3]["type"] != "num"):
-        print("TYPE ERROR: variable", p[1],'and',p[3], 'not matching type.')
+    if(p[3]["type"] != "num" and p[3]["type"] != "int"):
+        print("TYPE ERROR: variable", p[1],'and',p[3]["type"], 'not matching type.')
     temp_name = ST.newtemp({"type" : "num"})
+    TAC.emit('Arithmetic',[p[2],temp_name,p[1],p[3]["place"]])
+    p[0] = {"place": temp_name, "type": "num"}
+
+def p_math_rightside_2(p):
+    '''rightside : TYPE_INTEGER math rightside'''
+    if(p[3]["type"] != "num" and p[3]["type"] != "int"):
+        print("TYPE ERROR: variable", p[1],'and',p[3], 'not matching type.')
+    temp_name = ST.newtemp({"type" : p[3]["type"]})
     TAC.emit('Arithmetic',[p[2],temp_name,p[1],p[3]["place"]])
     p[0] = {"place": temp_name, "type": p[3]["type"]}
 
-def p_math_rightside_2(p):
+def p_math_rightside_3(p):
+    '''rightside : IDENTIFIER math rightside '''
+    var_dict = ST.varlookup(p[1])
+    print var_dict
+    if(var_dict == False):
+        var_dict = globalST.varlookup(p[1])
+        if (var_dict == False):
+            print("ERROR: variable", p[1],'is not defined.')
+            TAC.error = True
+    temp_name = ST.newtemp({"type" : p[3]["type"]})
+    TAC.emit('Arithmetic',[p[2],temp_name, p[1], p[3]["place"]])
+    p[0] = {"place": temp_name, "type": p[3]["type"]}
+
+def p_math_rightside_4(p):
+    '''rightside : BR_LCIR rightside BR_RCIR '''
+    p[0] = p[2]
+
+def p_math_rightside_5(p):
+    '''rightside : IDENTIFIER '''
+    var_dict = ST.varlookup(p[1])
+    if(var_dict == False):
+        var_dict = globalST.varlookup(p[1])
+        if (var_dict == False):
+            print("ERROR: variable", p[1],'is not defined.')
+            TAC.error = True
+    p[0] = {"place":p[1], "type": var_dict["type"]} 
+
+def p_math_rightside_6(p):
     '''rightside : any_type'''
     p[0] = p[1]
 
-def p_math(p):
-    '''math : OP_PLUS
-            | OP_MINUS
-            | OP_MULT
-            | OP_DIVIDE
-            | OP_EXPO
-            | OP_REMDR'''
+def p_math_1(p):
+    '''math : OP_PLUS'''
+    p[0] = p[1]
+        
+def p_math_2(p):
+    '''math : OP_MINUS'''
+    p[0] = p[1]
+        
+def p_math_3(p):
+    '''math : OP_MULT'''
+    p[0] = p[1]
+        
+def p_math_4(p):
+    '''math : OP_DIVIDE'''
+    p[0] = p[1]
+        
+def p_math_5(p):
+    '''math : OP_EXPO'''
+    p[0] = p[1]
+        
+def p_math_6(p):
+    '''math : OP_REMDR'''
     p[0] = p[1]
 
 def p_any_type_1(p):
@@ -93,6 +136,7 @@ def p_any_type_4(p):
 
 def p_error(p):
     print("Syntax error in input!")
+    print p
 
 
 
@@ -124,8 +168,9 @@ for line in myfile.readlines():
 TAC = tac.TAC()
 
 result = yacc.parse(pdata)
+myfile.close()
 print "global symboltable";
 print ST.vardict
 print ST.funcdict
 if (TAC.error == False):
-    TAC.printTAC()
+    TAC.printTAC(filename)
